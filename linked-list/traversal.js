6fcdb41c45338fa677f977667cc7fce0eb46be46
Franco Di Leo <dileo.francoj@gmail.com>
2022-08-30 18:13:51 -0300

    chore: recursive way in traverse linked list

--- linked-list/traversal.js
+++ linked-list/traversal.js
@@ -1,13 +1,16 @@
 const { Node } = require("./Node");
 
-const  a = new Node("a");
-const  b = new Node("b");
-const  c = new Node("c");
-const  d = new Node("d");
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
 
 a.next = b;
 b.next = c;
 c.next = d;
+
+/* Time Complexity: O(n) */
+/* Space complexity O(1) */
 
 function traverseLinkedList(head) {
   let current = head;
@@ -18,5 +21,14 @@ function traverseLinkedList(head) {
   return;
 }
 
-traverseLinkedList(a)
+/* Time Complexity: O(n) */
+/* Space complexity O(n) because the callstack is filling up */
 
+function traverseLinkedListRecursive(head) {
+  if (!head) return;
+  console.log("[Recursive] Current val: ", head.val);
+  traverseLinkedListRecursive(head.next);
+}
+
+traverseLinkedList(a);
+traverseLinkedListRecursive(a);
